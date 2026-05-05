aa202bd6e065ad1e27bf814ed800135fead0dab1
Prabha Sapkota <prabhasapkota3456@gmail.com>
2026-05-05 23:08:47 +0545

    Import configuration variables from config module

--- ai_processor.py
+++ ai_processor.py
@@ -1,3 +1,13 @@
 import logging
 from openai import OpenAI, APIError, AuthenticationError, RateLimitError
 
+from config import (
+    OPENAI_API_KEY,
+    LLM_MODEL,
+    LLM_MAX_TOKENS,
+    LLM_TEMPERATURE,
+    SYSTEM_PROMPT,
+)
+
+logger = logging.getLogger(__name__)
+
